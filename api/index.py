eda6d967035c088488255da109397e59c16b0b40
Autostack <ameenosman1104@gmail.com>
2026-06-13 00:26:43 +0200

    Fix api/index.py import paths

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
+import sys
+import os
+
+# Add parent directory to path so we can import run and app modules
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from run import app
